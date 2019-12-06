a6481f9dc0718af922180458986a5404e8dc0ad2
Бондарь Николай <Bufod>
2019-12-06 19:49:51 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,25 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Child child1 = new Child("Олег", "Адаменко", 15,
                 "мужской", "Русский", "Школа 39");
         Adult adult1 = new Adult("Петр", "Первый", 26, "мужской", "Русский",
                 "Google", "6","Владелец");
 
-        System.out.println(child1);
-        System.out.println(adult1);
+        FileWriter fout = null;
+        try {
+            fout = new FileWriter("output.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fout.write(child1.toString());
+        fout.write(adult1.toString());
+        fout.close();
     }
 }
